2b26b139cfafd0d5468cc78cdb85e4dba81716b5
bitbytejoy <bitbytejoy@gmail.com>
2025-03-21 15:04:23 +0100

    getSize fix

--- src/getSize.ts
+++ src/getSize.ts
@@ -1,19 +1,27 @@
 import {appendToBody} from "./appendToBody.ts";
 import {appendToElement} from "./appendToElement.ts";
 import {isElementInDom} from "./isElementInDom.ts";
+import {removeElement} from "./removeElement.ts";
 
 export function getSize(element: Element): [number, number] {
     const parentNode = element.parentElement;
 
+    let needsRemoval = false;
+
     if (!isElementInDom(element)) {
         appendToBody(element);
+        needsRemoval = true;
     }
 
     const rect = element.getBoundingClientRect();
     const size: [number, number] = [rect.width, rect.height];
 
-    if (parentNode) {
-        appendToElement(parentNode, element);
+    if (needsRemoval) {
+        if (parentNode) {
+            appendToElement(parentNode, element);
+        } else {
+            removeElement(element);
+        }
     }
 
     return size;
