c0ee3a1aadffeeeed1255baf66900203b68ae41f
bitbytejoy <bitbytejoy@gmail.com>
2025-03-21 22:09:20 +0100

    disableForm & enableForm

--- src/disableForm.ts
+++ src/disableForm.ts
@@ -1,7 +1,11 @@
+import {addClass} from "./addClass.ts";
+
 export function disableForm(form: HTMLFormElement): void {
     form.querySelectorAll("input, button, select, textarea").forEach(element => {
         if (element instanceof HTMLInputElement || element instanceof HTMLButtonElement || element instanceof HTMLSelectElement || element instanceof HTMLTextAreaElement) {
             element.disabled = true;
         }
     });
+
+    addClass(form, "disabled");
 }
--- src/enableForm.ts
+++ src/enableForm.ts
@@ -1,7 +1,11 @@
+import {removeClass} from "./removeClass.ts";
+
 export function enableForm(form: HTMLFormElement): void {
     form.querySelectorAll("input, button, select, textarea").forEach(element => {
         if (element instanceof HTMLInputElement || element instanceof HTMLButtonElement || element instanceof HTMLSelectElement || element instanceof HTMLTextAreaElement) {
             element.disabled = false;
         }
     });
+
+    removeClass(form, "disabled");
 }
