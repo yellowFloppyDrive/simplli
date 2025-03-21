2f390274cbe4bf82f6620333b0416a86403fd1a3
bitbytejoy <bitbytejoy@gmail.com>
2025-03-21 12:47:35 +0100

    innerText improvement

--- src/innerText.ts
+++ src/innerText.ts
@@ -1,3 +1,9 @@
-export function innerText(element: HTMLElement, text: string): void {
+export function innerText(element: HTMLElement, text?: string): string {
+    if (text === undefined) {
+        return element.innerText;
+    }
+
     element.innerText = text;
+
+    return text;
 }
